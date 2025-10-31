e06f481fb073e88fcaf06e92ea6817c6470986a7
Fiyinfoluwa Osokoya <osokoyafiyinfoluwa@gmail.com>
2025-10-31 16:15:57 +0100

    Remove unused comment

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ router.post('/search', async(req,res)=>{
   // const description = req.body.description;
   const {query, description} = req.body;
 
-  // TODO: Use AI to enhance search query
   // const searchQuery = query + " " + description ...This is the older way
   const searchQuery = `${query} ${description}`;
 
